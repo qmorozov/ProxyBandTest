31b4aa74183c4488245bf3b63c7e8380e5df845f
Andrew Morozov <andrew@Andrews-Laptop.local>
2023-02-11 21:00:25 +0100

    fetchAlbums

--- src/modules/Album/index.tsx
+++ src/modules/Album/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { useAppDispatch } from '../../hooks/useAppRedux';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect } from 'react';
+import {
+  getAlbumsError,
+  getAlbumsStart,
+  getAlbumsSuccess,
+} from './store/album';
 
 const Album = () => {
-  return <div></div>;
+  const dispatch = useAppDispatch();
+  const { id } = useParams<{ id: string }>();
+
+  const fetchAlbums = async (id: string) => {
+    dispatch(getAlbumsStart());
+
+    try {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/albums?userId=${id}`
+      );
+      const posts = response.data;
+      dispatch(getAlbumsSuccess(posts));
+    } catch (error: any) {
+      dispatch(getAlbumsError(error.message));
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetchAlbums(id);
+    }
+  }, [id]);
+
+  return <></>;
 };
 
 export default Album;
